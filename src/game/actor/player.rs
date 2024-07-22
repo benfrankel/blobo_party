7a280978831e89de3c6eeb0f89905ee9c810fd0a
Ben Frankel <ben.frankel7@gmail.com>
2024-07-21 17:05:20 -0700

    Remove facing indicator from enemies

--- src/game/actor/player.rs
+++ src/game/actor/player.rs
@@ -1,7 +1,10 @@
+use bevy::math::vec2;
+use bevy::math::vec3;
 use bevy::prelude::*;
 
 use crate::game::actor::actor_helper;
 use crate::game::actor::facing::FaceCursor;
+use crate::game::actor::facing::FacingIndicator;
 use crate::game::deck_dock::DrawDeck;
 use crate::util::prelude::*;
 
@@ -20,5 +23,13 @@ impl Configure for IsPlayer {
 }
 
 pub fn player(entity: EntityWorldMut) {
-    actor_helper(entity, None).insert((IsPlayer, DrawDeck, FaceCursor));
+    actor_helper(entity, None)
+        .insert((IsPlayer, DrawDeck, FaceCursor))
+        .with_children(|children| {
+            children
+                .spawn_with(FacingIndicator {
+                    radius: vec2(6.0, 5.0),
+                })
+                .insert(Transform::from_translation(vec3(0.0, -0.5, 2.0)));
+        });
 }
